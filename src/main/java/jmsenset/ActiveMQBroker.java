895e5b03c6129f132850dc4b93807e7822037b52
alkostylew <aleks.kostylew@gmail.com>
2020-05-23 14:23:21 -0300

    Added local broker

--- src/main/java/jmsenset/ActiveMQBroker.java
+++ src/main/java/jmsenset/ActiveMQBroker.java
@@ -1,8 +1,18 @@
 package jmsenset;
 
+import org.apache.activemq.broker.BrokerService;
+
 public class ActiveMQBroker {
 
 	public static void main(String[] args) {
-		
+		BrokerService brokerService = new BrokerService();
+		try {
+			brokerService.addConnector("tcp://0.0.0.0:61616");
+			brokerService.setPersistent(false);
+			brokerService.start();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
